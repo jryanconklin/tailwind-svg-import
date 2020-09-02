b4527c6383d7f49ecc63e117b0e4056b176e1042
J. Ryan Conklin <jryanconklin@gmail.com>
2020-09-02 16:25:26 -0700

    refactor: simplify readsvg func

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ const defaultOptions = require('./src/defaultOptions');
 function readSVG(dir) {
   const tail = '.svg';
   // Read the directory, get the files.
-  const files = fs.readdirSync(dir).filter((file) => file.includes(tail));
-  return files.reduce((accumulator, filePath) => {
-    const name = filePath.replace(tail, '');
-    try {
-      accumulator[name] = svgToDataUri(
-        fs.readFileSync(path.join(dir, filePath), 'utf8')
-      );
-    } catch (err) {
-      console.log(`file read error ${filePath}: ${err}`);
-    }
-    return accumulator;
-  }, {});
+  return fs
+    .readdirSync(dir)
+    .filter((file) => file.includes(tail))
+    .reduce((acc, filePath) => {
+      const name = filePath.replace(tail, '');
+      try {
+        acc[name] = svgToDataUri(
+          fs.readFileSync(path.join(dir, filePath), 'utf8')
+        );
+      } catch (err) {
+        console.log(`file read error ${filePath}: ${err}`);
+      }
+      return acc;
+    }, {});
 }
 
 module.exports = {};
