333a1dbfe721c12a18c7246de73210e831098a4e
J. Ryan Conklin <jryanconklin@gmail.com>
2020-09-02 16:13:31 -0700

    fix: add filter to file read

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const defaultOptions = require('./src/defaultOptions');
 function readSVG(dir) {
   const tail = '.svg';
   // Read the directory, get the files.
-  const files = fs.readdirSync(dir);
+  const files = fs.readdirSync(dir).filter((file) => file.includes(tail));
   return files.reduce((accumulator, filePath) => {
     const name = filePath.replace(tail, '');
     try {
