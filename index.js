41748432994d386e61de425993e4187556b7aed3
J. Ryan Conklin <jryanconklin@gmail.com>
2020-09-02 15:49:57 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// Module Dependencies
+const path = require('path');
+const fs = require('fs');
+const svgToDataUri = require('mini-svg-data-uri');
+
+// Tailwind Options
+const defaultOptions = require('./src/defaultOptions');
+
+function readSVG(dir) {
+  const tail = '.svg';
+  // Read the directory, get the files.
+  const files = fs.readdirSync(dir);
+  return files.reduce((accumulator, filePath) => {
+    const name = filePath.replace(tail, '');
+    try {
+      accumulator[name] = svgToDataUri(
+        fs.readFileSync(path.join(dir, filePath), 'utf8')
+      );
+    } catch (err) {
+      console.log(`file read error ${filePath}: ${err}`);
+    }
+    return accumulator;
+  }, {});
+}
+
+module.exports = {};
---
+++ src/defaultOptions.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/resolveConfig')(
+  require('tailwindcss/defaultConfig')
+).theme;
+
+const path = require('path');
+
+const colors = {
+  black: defaultTheme.colors.black,
+  white: defaultTheme.colors.white,
+  gray: defaultTheme.colors.gray[500],
+  red: defaultTheme.colors.red[500],
+  orange: defaultTheme.colors.orange[500],
+  yellow: defaultTheme.colors.yellow[500],
+  green: defaultTheme.colors.green[500],
+  teal: defaultTheme.colors.teal[500],
+  blue: defaultTheme.colors.blue[500],
+  indigo: defaultTheme.colors.indigo[500],
+  purple: defaultTheme.colors.purple[500],
+  pink: defaultTheme.colors.pink[500],
+};
+
+module.exports = {
+  dir: path.resolve(__dirname, '../heroicons/optimized/outline/'),
+  colors,
+};
